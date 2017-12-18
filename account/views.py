bd407abe5988dd931801523ad25c9b0dfc06f188
Gan <1354410847@qq.com>
2017-12-18 23:35:25 +0800

    添加了用户登录、注销，修改密码功能。未实现密码重置功能

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -24,3 +25,8 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form, })
 # Create your views here.
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {"section": 'dashboard'})
--- social/settings.py
+++ social/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'common_static'),
 )
+
+LOGIN_REDIRECT_URL = reverse_lazy('account:dashboard')
+LOGIN_URL = reverse_lazy('account:login')
+LOGOUT_URL = reverse_lazy('account:logout')
