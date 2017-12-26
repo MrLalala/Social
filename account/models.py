154ad09712535fb438fdb93143dba4f06a741910
Gan <1354410847@qq.com>
2017-12-26 21:38:06 +0800

    增加一点东西。

--- account/models.py
+++ account/models.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django import forms
 from django.conf import settings
 from django.contrib.auth.models import User
 
@@ -12,6 +11,7 @@ class Profile(models.Model):
     # 这里并没有直接使用User，而是通过一个名为AUTH_USER_MODEL来引用。
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     date_of_birth = models.DateField(blank=True, null=True)
+    # 通过ImageField里的upload_to指定了上传的文件的保存路径。
     photo = models.ImageField(upload_to='user/%Y/%m/%d', blank=True)
 
     def __str__(self):
